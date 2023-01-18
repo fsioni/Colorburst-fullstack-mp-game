97063955f2ae56bd5b492c73e62da5ac76c7f76b
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-01-18 17:12:19 +0100

    testing pre-commit hook

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,3 +14,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+console.log("Testing pre-commit hook");
