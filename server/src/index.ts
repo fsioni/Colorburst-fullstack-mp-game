83adcec87448b4ee5c38e1f92804458cad401f88
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-01-19 22:46:02 +0100

    misc: add cors on backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config({ path: "config_var.env" });
 
 const app: Express = express();
+app.use(cors());
+
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
