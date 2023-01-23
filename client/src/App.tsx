63e5d21c4febffb4961d575210182956a8b9f5c0
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-01-23 22:50:50 +0100

    Added types to io Socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-import io from "socket.io-client";
-const socket = io("http://localhost:3000");
+import * as io from "socket.io-client";
+
+const socket: io.Socket = io.io("http://localhost:3000");
 
 export default function App() {
   return <div>Client</div>;
