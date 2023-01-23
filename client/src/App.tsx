1c6304b90d02b800ead61e67ac3d64c9cba92cf1
LeXillal <julien38.b@gmail.com>
2023-01-23 16:48:32 +0100

    socket loged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 export default function App() {
   return <div>Client</div>;
