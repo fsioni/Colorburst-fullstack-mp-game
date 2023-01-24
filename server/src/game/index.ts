1a89fe927b4539d18a8636150c180427e1a441cd
LeXillal <julien38.b@gmail.com>
2023-01-24 09:14:51 +0100

    directionChange / logFunction

--- server/src/game/index.ts
+++ server/src/game/index.ts
@@ -2,6 +2,8 @@ import { Server, Socket } from "socket.io";
 import Settings from "../interfaces/Settings";
 import Player from "./player";
 
+let log = (...text: string[]) => console.log(`[Game] ${text.join(" ")}`);
+
 export default class Game {
   players: Player[];
   socket: any;
@@ -12,31 +14,29 @@ export default class Game {
     this.players = [];
     this.settings = settings;
     this.gameID = Math.random().toString(36).substring(7);
+    log = (...text: string[]) =>
+      console.log(`[Game:${this.gameID}] ${text.join(" ")}`);
   }
 
   // Ajouter le joueur à la partie
   addPlayer(socket: Socket) {
-    console.log("[Game] A player Join =>", socket.id);
+    log("A player Join =>", socket.id);
     this.players.push(new Player(socket.id));
     socket.join(this.gameID);
     handlePlayerEvents(socket, this);
-    console.log(
-      `[Game] There is curently ${this.players.length} players in the game.`
-    );
+    log(`There is curently ${this.players.length} players in the game.`);
   }
 }
 
 // Gérer tout les événements liés au joueur
 function handlePlayerEvents(socket: Socket, game: Game) {
-  socket.on("playerMove", (data: any) => {
-    console.log("playerMove", data);
+  socket.on("directionChange", (data: any) => {
+    log(`${socket.id} => directionChange`, data);
   });
 
   socket.on("disconnect", () => {
-    console.log("[Game] A player leave =>", socket.id);
+    log("A player leave =>", socket.id);
     game.players = game.players.filter((p) => p.name !== socket.id);
-    console.log(
-      `[Game] There is curently ${game.players.length} players in the game.`
-    );
+    log(`There is curently ${game.players.length} players in the game.`);
   });
 }
