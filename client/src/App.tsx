29e3977c49bf880ec8f6e7c4c19ecf6bd93e4c7a
LeXillal <julien38.b@gmail.com>
2023-01-24 09:13:26 +0100

    User move handle in front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,23 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 const socket = io("http://localhost:3000");
 
+const keysDirection: string[] = [
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+];
+console.log(keysDirection);
 export default function App() {
+  useEffect(() => {
+    // Keyboards events
+    document.addEventListener("keydown", (e) => {
+      console.log(e.key);
+      if (keysDirection.includes(e.key)) {
+        socket.emit("directionChange", keysDirection.indexOf(e.key));
+      }
+    });
+  });
+
   return <div>Client</div>;
 }
