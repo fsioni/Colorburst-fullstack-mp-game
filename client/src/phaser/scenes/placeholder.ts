ed7ed3d2f704af1aa1b87927dbc80985efda792b
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-01-29 00:54:35 +0100

    Controls experimentation

--- client/src/phaser/scenes/placeholder.ts
+++ client/src/phaser/scenes/placeholder.ts
@@ -7,6 +7,8 @@ import Phaser from "phaser";
  * @public
  */
 export class FirstGameScene extends Phaser.Scene {
+  private controls!: Phaser.Cameras.Controls.SmoothedKeyControl;
+
   constructor() {
     super("FirstGameScene");
     console.log("FirstGameScene.constructor()");
@@ -17,6 +19,22 @@ export class FirstGameScene extends Phaser.Scene {
   }
 
   create() {
+    const cursors = this.input.keyboard.createCursorKeys();
+
+    this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl({
+      camera: this.cameras.main,
+      left: cursors.left,
+      right: cursors.right,
+      up: cursors.up,
+      down: cursors.down,
+      acceleration: 0.06,
+      drag: 0.0005,
+      maxSpeed: 1.0,
+    });
+
+    const { width, height } = this.sys.canvas;
+    this.add.grid(width / 2, height / 2, width, height, 64, 64, 0x00b9f2); //.setAltFillStyle(0x016fce)//.setOutlineStyle();
+
     const graphics = this.add.graphics({ fillStyle: { color: 0xff0000 } });
 
     const circle = new Phaser.Geom.Circle(400, 300, 200);
@@ -36,7 +54,7 @@ export class FirstGameScene extends Phaser.Scene {
     });
   }
 
-  /*update() {
-
-    }*/
+  update(time: number, deltaTime: number) {
+    this.controls.update(deltaTime);
+  }
 }
