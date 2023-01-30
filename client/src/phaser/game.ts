3637f234c24894195965cc8f38e12df15a47c248
LeXillal <julien38.b@gmail.com>
2023-01-30 11:08:42 +0100

    export et window.phaserGame

--- client/src/phaser/game.ts
+++ client/src/phaser/game.ts
@@ -1,6 +1,8 @@
 import "phaser";
 import { config } from "./config";
 
-window.addEventListener("load", () => {
-  new Phaser.Game(config);
-});
+const phaserGame = new Phaser.Game(config);
+
+(window as any).phaserGame = phaserGame;
+
+export default phaserGame;
