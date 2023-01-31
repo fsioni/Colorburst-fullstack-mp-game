473e7e1ab015b01b8234144cc045270749e4bb30
Nothied <edouard.thinot@etu.univ-lyon1.fr>
2023-01-31 10:13:53 +0100

    Creat menuFrontEnd branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, FC } from "react";
 
 import io from "socket.io-client";
 const socket = io("http://localhost:3000");
 
-export default function App() {
-  return <div>Client</div>;
-}
+import HeadMenu from "./components/Menu/HeadMenu";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <HeadMenu />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Menu/HeadMenu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeadMenu.css";
+
+const HeadMenu = () => {
+  return (
+    <div className="header">
+      <span className="game__tile">ColorBurst</span>
+    </div>
+  );
+};
+
+export default HeadMenu;
