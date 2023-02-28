c7e2eba3280c59812c5ca5e9782b324f6ee432c4
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-28 17:22:11 +0100

    Display Header on top of the game

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { config } from "../phaser/config";
+import HeadMenu from "./Menu/Header/HeadMenu";
 
 function Game() {
   const [game, setGame] = useState<Phaser.Game>();
@@ -8,7 +9,12 @@ function Game() {
     setGame(new Phaser.Game(config));
   }, []);
 
-  return <div id="phaser"></div>;
+  return (
+    <>
+      <HeadMenu />
+      <div id="phaser"></div>
+    </>
+  );
 }
 
 export default Game;
