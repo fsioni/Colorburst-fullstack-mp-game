4e48b42c0107055ac02089b5e3010255d7311bdb
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-28 17:21:53 +0100

    Check if user is connected to firebase -> display Game

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,19 +4,39 @@ import HeadMenu from "./Menu/Header/HeadMenu";
 import InputField from "./Menu/InputField";
 import Rooms from "./Menu/Rooms/Rooms";
 import Statistics from "./Menu/Stats/Statistics";
+import Game from "./Game";
+import app from "../Firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const App: FC = () => {
+  const [isConnected, setIsConnected] = React.useState(false);
+
+  const auth = getAuth(app);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsConnected(true);
+    } else {
+      setIsConnected(false);
+    }
+  });
+
   return (
-    <div className="App">
-      <HeadMenu />
-      <div className="inputF-container">
-        <InputField username={""} isLoged={0} />
-      </div>
-      <div className="statsAndRooms-container">
-        <Rooms />
-        <Statistics />
-      </div>
-    </div>
+    <>
+      {isConnected ? (
+        <Game />
+      ) : (
+        <div className="App">
+          <HeadMenu />
+          <div className="inputF-container">
+            <InputField username={""} isLoged={0} />
+          </div>
+          <div className="statsAndRooms-container">
+            <Rooms />
+            <Statistics />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
