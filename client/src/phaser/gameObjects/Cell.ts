7d5a9f0f15cf3b0b2a531f5b5a53e4652cc77853
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-03-28 14:56:19 +0200

    UI tweaks

--- client/src/phaser/gameObjects/Cell.ts
+++ client/src/phaser/gameObjects/Cell.ts
@@ -3,6 +3,7 @@ export default class Cell extends Phaser.GameObjects.Sprite {
   occupedBy: string | null = null;
   constructor(scene: Phaser.Scene, grid: AlignGrid, i: number, j: number) {
     super(scene, 0, 0, "boardCells");
+    this.setAlpha(0.95);
     scene.add.existing(this);
     this.setScale(15);
     this.setFrame(0, true);
